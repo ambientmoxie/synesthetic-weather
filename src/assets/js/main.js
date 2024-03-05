5394ed7d2fecc32ae14718064cf9b6d7d1638a44
ambientmoxie <contact@maximebenoit.fr>
2024-03-05 19:19:44 +0100

    code refactored

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,56 @@
 import "../scss/style.scss";
-import { getDataAndDisplayGradient, apiConfig } from "./weatherApi";
+import { animateBackground, setGradientBackground, setUI } from "./ui";
 
-// TODO: refactor code. Compare zip codes.
+// API URL
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+// Api base configuration
+export const apiConfig = {
+  zip: 75000,
+  cd: "FR",
+  units: "metric",
+  key: import.meta.env.VITE_API_KEY,
+};
+
+// Fetch the data
+const fetchWeather = async (zip, country, units, apiKey) => {
+  const url = `${BASE_URL}?zip=${zip},${country}&units=${units}&appid=${apiKey}`;
+  const resp = await fetch(url);
+  if (!resp.ok) throw new Error(resp.statusText);
+  return await resp.json();
+};
+
+// Refactor the data in order to create a more usable object
+const transformWeatherData = (resp) => {
+  return {
+    city: resp.name,
+    temperature: Math.floor(resp.main.temp),
+    weather: resp.weather[0].description,
+    windSpeed: resp.wind.speed,
+    humidity: resp.main.humidity,
+  };
+};
+
+// Wait for the data, create the data object and create the scene
+const createScene = () => {
+  fetchWeather(apiConfig.zip, apiConfig.cd, apiConfig.units, apiConfig.key)
+    .then((resp) => {
+      let weatherObject = transformWeatherData(resp);
+      setGradientBackground(weatherObject);
+      animateBackground(weatherObject.windSpeed);
+      setUI(weatherObject);
+    })
+    .catch(console.error);
+};
+
+const updateOnClick = (apiConfig) => {
+  document.getElementsByTagName("button")[0].addEventListener("click", () => {
+    apiConfig.zip = document.getElementById("zip").value;
+    createScene();
+  });
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  getDataAndDisplayGradient();
-
-  document.getElementsByTagName("button")[0].addEventListener("click", () => {
-    apiConfig.zip = document.getElementById("zip").value; // Update zip code
-    getDataAndDisplayGradient();
-  });
+  createScene();
+  updateOnClick(apiConfig);
 });
-
-
-
--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -2,13 +2,8 @@ import { COLORS, remap } from "./utils";
 let currentBackgroundPosition = 0;
 let animationFrameId;
 
-let colorTemperature;
-let colorWeather;
-let colorWind;
-
-// Animate background position using requestAnimationFrame
-
-const animateBackground = (windSpeed) => {
+// Animate background position
+export const animateBackground = (windSpeed) => {
   //! TODO: Find a better way to control windspeed value
 
   let windSpeedRemap = remap(windSpeed, 0, 200, 0, 60);
@@ -34,34 +29,37 @@ const animateBackground = (windSpeed) => {
   animationFrameId = requestAnimationFrame(animate);
 };
 
-// Set the background values
-
-const setGradientBackground = (weatherObject) => {
+// Set the gradient color
+export const setGradientBackground = (weatherObject) => {
   // Handle negative temperature
   const temperatureIndex =
     weatherObject.temperature < 0 ? 0 : weatherObject.temperature;
 
-  colorTemperature = COLORS[Math.round(temperatureIndex)];
-  colorWeather = COLORS[Math.round(weatherObject.weather.length)];
-  colorWind = COLORS[Math.round(weatherObject.windSpeed)];
-
-  console.log(colorTemperature);
+  let colorTemperature = COLORS[Math.round(temperatureIndex)];
+  let colorWeather = COLORS[Math.round(weatherObject.weather.length)];
+  let colorWind = COLORS[Math.round(weatherObject.windSpeed)];
 
   document.body.style.backgroundImage = `linear-gradient( to top, ${colorTemperature}, ${colorWeather}, ${colorWind}, ${colorTemperature})`;
 };
 
-// Create the whole composition
+// Set the text
+export const setUI = (weatherObject) => {
+  document.querySelector(
+    "#top-text"
+  ).innerHTML = `<p>${weatherObject.city}</p><p>${weatherObject.temperature}° celcius</p><p>${weatherObject.weather}</p><p>wind ${weatherObject.windSpeed} mph</p><p>humidity ${weatherObject.humidity}%</p>`;
+  document.querySelector(
+    "#bottom-text"
+  ).innerText = `THE COLOR AND THE SPEED OF THE GRADIENT WERE GENERATED USING THE VALUES BELOW`;
+};
 
+// Create the whole composition
 export const updateUI = (weatherObject) => {
-  animateBackground(weatherObject.windSpeed);
   setGradientBackground(weatherObject);
+  // animateBackground(weatherObject.windSpeed);
 
   document.querySelector(
     "#top-text"
   ).innerHTML = `<p>${weatherObject.city}</p><p>${weatherObject.temperature}° celcius</p><p>${weatherObject.weather}</p><p>wind ${weatherObject.windSpeed} mph</p><p>humidity ${weatherObject.humidity}%</p>`;
-  // document.querySelector(
-  //   "#bottom-text"
-  // ).innerHTML = `${colorTemperature} <br/> ${colorWeather} <br/> ${colorWind} <br/>`;
   document.querySelector(
     "#bottom-text"
   ).innerText = `THE COLOR AND THE SPEED OF THE GRADIENT WERE GENERATED USING THE VALUES BELOW`;
